5e84209bcb3ac47cdff60bf153edafea392eaae8
Gülşen <gulsenk48@hotmail.com>
2024-11-01 22:22:03 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] numbers = new int[10]; //Create new array
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        foreach (var item in numbers) //Write array
+        {
+            Console.WriteLine(item);
+        }
+
+        //Create a new array and copy previous array
+        Console.Write("Enter a new value");
+        int newValue = Convert.ToInt32(Console.ReadLine());
+
+        int[] newNumbers = new int[11]; //New array
+
+        for (int i = 0; i < numbers.Length; i++) //Every value copy new array
+        {
+            newNumbers[i] = numbers[i]; //Copy values
+        }
+
+        newNumbers[10] = newValue; //Add new index
+
+        Array.Sort(newNumbers); //Sorted array
+        foreach (var item in newNumbers) //Write new array
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
